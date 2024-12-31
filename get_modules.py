9050cce1d7072f33dbe28c2fe69e10676c990256
Henny <2669753313@qq.com>
2024-12-31 21:05:15 +0800

    Deleted all

--- get_modules.py
+++
@@ -1,9 +0,0 @@
-class GetModules(object):
-
-    @staticmethod
-    def get_modules():
-        modules_list = ['PyAutoGUI', 'PyGetWindow', 'pynput', 'requests', 'pypiwin32', 'keyboard']
-        modules = [module for module in set(modules_list)]
-
-        modules.sort()
-        return modules
